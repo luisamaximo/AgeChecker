9912cdf42f4c9562606bcfab85a65c7d33693e53
Luisa <.oliveira@autotrader.co.uk>
2023-11-30 15:02:25 +0000

    chore: change const oldAge to Age

--- src/AgeChecker.test.js
+++ src/AgeChecker.test.js
@@ -3,15 +3,15 @@ import ageChecker from './AgeChecker';
 describe('Date of Birth Validation', () => {
     
     test('should return false if age > 100', () => {
-        const oldAge = 150;
-        const test = ageChecker(oldAge)
+        const age = 150;
+        const test = ageChecker(age)
         expect(test).toBe(false)
     }
     )
 
     test('should return false if age < 18', () => {
-        const youngAge = 17;
-        const test = ageChecker(youngAge)
+        const age = 17;
+        const test = ageChecker(age)
         expect(test).toBe(false)
     })
 
